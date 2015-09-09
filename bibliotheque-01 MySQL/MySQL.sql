da20ef81bf729548dad27dcfbef8acc8a35c4f09
Marc-Éric Boury <Marc-Eric.Boury.1@collegeahuntsic.qc.ca>
2015-09-09 13:20:26 -0400

    MySQL part1

--- bibliotheque-01 MySQL/MySQL.sql
+++ bibliotheque-01 MySQL/MySQL.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS membre CASCADE;
 DROP TABLE IF EXISTS livre CASCADE;
+DROP TABLE IF EXISTS membre CASCADE;
 
 CREATE TABLE membre (
 idMembre INTEGER(3) check(idMembre > 0),
@@ -9,3 +9,13 @@ limitePret INTEGER(2), check(limitePret > 0 and limitePret <= 10),
 nbpret INTEGER(2) default 0 check(nbpret >= 0),
 CONSTRAINT cleMembre PRIMARY KEY (idMembre),
 CONSTRAINT limiteNbPret check(nbpret <= limitePret));
+
+CREATE TABLE livre (
+idLivre         INTEGER(3) check(idLivre > 0),
+titre           varchar(10) NOT NULL,
+auteur          varchar(10) NOT NULL,
+dateAcquisition DATE not null,
+idMembre        INTEGER(3),
+datePret        DATE,
+CONSTRAINT cleLivre PRIMARY KEY (idLivre),
+CONSTRAINT refPretMembre FOREIGN KEY (idMembre) REFERENCES membre (idMembre));
