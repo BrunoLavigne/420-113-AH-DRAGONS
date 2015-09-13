cead0fc4a61a46c5d178308db7856a91617c6408
bruno <bruno@baSs-PC>
2015-09-12 20:57:32 -0400

    Corrected indentation des fichiers SQL

--- bibliotheque-01-Oracle/Oracle.sql
+++ bibliotheque-01-Oracle/Oracle.sql
@@ -1,37 +1,37 @@
-DROP TABLE reservation CASCADE CONSTRAINTS PURGE;
-DROP TABLE livre CASCADE CONSTRAINTS PURGE;
-DROP TABLE membre CASCADE CONSTRAINTS PURGE;
+DROP TABLE reservation 	CASCADE CONSTRAINTS PURGE;
+DROP TABLE livre 		CASCADE CONSTRAINTS PURGE;
+DROP TABLE membre 		CASCADE CONSTRAINTS PURGE;
 
 CREATE TABLE membre (
-idMembre        number(3) CHECK(idMembre > 0),
-nom             varchar(10) NOT NULL,
-telephone       number(10),
-limitePret      number(2) CHECK(limitePret > 0 and limitePret <= 10),
-nbpret          number(2) default 0 CHECK(nbpret >= 0),
-PRIMARY KEY (idMembre),
-CONSTRAINT limiteNbPret check(nbpret <= limitePret)
+	idMembre        NUMBER(3) 		CHECK(idMembre > 0),
+	nom             VARCHAR(10) 	NOT NULL,
+	telephone       NUMBER(10),
+	limitePret      NUMBER(2) 		CHECK(limitePret > 0 and limitePret <= 10),
+	nbpret          NUMBER(2) 		DEFAULT 0 CHECK(nbpret >= 0),
+	PRIMARY KEY 	(idMembre),
+	CONSTRAINT 		limiteNbPret 	CHECK(nbpret <= limitePret)
 );
 
 CREATE TABLE livre (
-idLivre         number(3) check(idLivre > 0),
-titre           varchar(10) NOT NULL,
-auteur          varchar(10) NOT NULL,
-dateAcquisition date not null,
-idMembre        number(3),
-datePret        date,
-CONSTRAINT cleLivre PRIMARY KEY (idLivre),
-CONSTRAINT refPretMembre FOREIGN KEY (idMembre) REFERENCES membre
+	idLivre         NUMBER(3) 		CHECK(idLivre > 0),
+	titre           VARCHAR(10) 	NOT NULL,
+	auteur          VARCHAR(10) 	NOT NULL,
+	dateAcquisition 				DATE NOT NULL,
+	idMembre        NUMBER(3),
+	datePret        DATE,
+	CONSTRAINT 		cleLivre 		PRIMARY KEY (idLivre),
+	CONSTRAINT 		refPretMembre 	FOREIGN KEY (idMembre) REFERENCES membre
 );
 
 CREATE TABLE reservation (
-idReservation   number(3),
-idMembre        number(3),
-idLivre         number(3),
-dateReservation date,
-CONSTRAINT cleReservation PRIMARY KEY (idReservation),
-CONSTRAINT cleCandidateReservation UNIQUE (idMembre,idLivre),
-CONSTRAINT refReservationMembre FOREIGN KEY (idMembre) REFERENCES membre
-  ON DELETE CASCADE,
-CONSTRAINT refReservationLivre FOREIGN KEY (idLivre) REFERENCES livre
-  ON DELETE CASCADE
+	idReservation   NUMBER(3),
+	idMembre        NUMBER(3),
+	idLivre         NUMBER(3),
+	dateReservation DATE,
+	CONSTRAINT 		cleReservation 			PRIMARY KEY (idReservation),
+	CONSTRAINT 		cleCandidateReservation UNIQUE (idMembre,idLivre),
+	CONSTRAINT 		refReservationMembre 	FOREIGN KEY (idMembre) REFERENCES membre
+	  ON DELETE CASCADE,
+	CONSTRAINT 		refReservationLivre 	FOREIGN KEY (idLivre) REFERENCES livre
+	  ON DELETE CASCADE
 );
