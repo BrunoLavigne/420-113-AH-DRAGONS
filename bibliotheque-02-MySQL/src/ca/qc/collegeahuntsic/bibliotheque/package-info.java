af68709f5d404b4cc6428a830b06d35d48c5608e
Marc-Éric Boury <Marc-Eric.Boury.1@collegeahuntsic.qc.ca>
2015-09-16 13:55:21 -0400

    Version fonctionnelle après le cours de mercredi 16 sept. Incluant Bibliothèque - 02 - MySQL

---
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/package-info.java
@@ -0,0 +1,10 @@
+// Fichier package-info.java
+// Auteur : Marc-Éric Boury
+// Date de création : 2015-09-16
+/**
+ * TODO Auto-generated class javadoc
+ *
+ * @author Marc-Éric Boury
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque;
