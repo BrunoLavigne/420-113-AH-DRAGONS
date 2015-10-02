13b635d3d33b3b93113f683f68d17af56648541e
Bruno Lavigne <bruno.lavigne@gmail.com>
2015-10-02 02:30:53 -0400

    modification organisation et javadoc membreService

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/MembreService.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/MembreService.java
@@ -5,7 +5,6 @@
 package ca.qc.collegeahuntsic.bibliotheque.service;
 
 import java.util.List;
-
 import ca.qc.collegeahuntsic.bibliotheque.dao.MembreDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dao.ReservationDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
@@ -29,247 +28,263 @@ import ca.qc.collegeahuntsic.bibliotheque.exception.ServiceException;
  */
 public class MembreService extends Services {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private MembreDAO membre;
+    private MembreDAO membreDAO;
 
-	private ReservationDAO reservation;
+    private ReservationDAO reservationDAO;
 
-	/**
-	 * Création d'une instance
-	 *
-	 * @param membre
-	 * @param reservation
-	 */
-	public MembreService(MembreDAO membre, ReservationDAO reservation) {
-		setMembre(membre);
-		setReservation(reservation);
-	}
+    /**
+     * Création d'une instance
+     *
+     * @param membre
+     * @param reservation
+     */
+    public MembreService(MembreDAO membre,
+        ReservationDAO reservation) {
+        setMembreDAO(membre);
+        setReservationDAO(reservation);
+    }
 
-	/**
-	 *
-	 * Vérifie si un membre existe
-	 *
-	 * @param idMembre
-	 * @return
-	 * @throws ServiceException
-	 */
-	public boolean existe(int idMembre) throws ServiceException {
+    // Opérations CRUD
 
-		// Un membre est-il trouvé? Si null, non: donc false
-		try {
-			return getMembre().read(idMembre) != null ? true : false;
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
-	}
+    /**
+     *
+     * Appel la méthode ADD de membreDAO
+     *
+     * @param membreDTO Le membre à ajouter à la base de données
+     * @throws ServiceException En cas d'erreur lors de l'appel au DAO, une exception est levée.
+     */
+    public void add(MembreDTO membreDTO) throws ServiceException {
 
-	/**
-	 * Ajout d'un nouveau membre dans la base de données. Si elle existe déjà,
-	 * une exception est levée.
-	 *
-	 * @param idMembre
-	 * @param nom
-	 * @param telephone
-	 * @param limitePret
-	 * @throws ServiceException
-	 */
-	public void inscrire(int idMembre, String nom, long telephone, int limitePret) throws ServiceException {
+        try {
+            getMembreDAO().add(membreDTO);
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
+    }
 
-		try {
+    /**
+     *
+     * Appel la méthode READ de le membreDAO
+     *
+     * @param idMembre Le ID du membre pour lequel nous désirons avoir des informations
+     * @return MembreDTO Le membre dont L'ID est spécifié
+     * @throws ServiceException En cas d'erreur lors de l'appel au DAO, une exception est levée.
+     */
+    public MembreDTO read(int idMembre) throws ServiceException {
 
-			if (getMembre().read(idMembre) != null) {
+        try {
+            return getMembreDAO().read(idMembre);
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
+    }
 
-				MembreDTO nouveauMembre = new MembreDTO();
-				nouveauMembre.setIdMembre(idMembre);
-				nouveauMembre.setNom(nom);
-				nouveauMembre.setTelephone(telephone);
-				nouveauMembre.setLimitePret(limitePret);
+    /**
+     *
+     * Appel de la méthode UPDATE de membreDAO
+     *
+     * @param membreDTO Le membre pour lequel nous désirons effectuer des modifications
+     * @throws ServiceException En cas d'erreur lors de l'appel au DAO, une exception est levée.
+     */
+    public void update(MembreDTO membreDTO) throws ServiceException {
 
-				add(nouveauMembre);
-			} else {
-				throw new ServiceException("Un membre avec l'id " + idMembre + " existe déjà");
-			}
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
+        try {
+            getMembreDAO().update(membreDTO);
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
+    }
 
-	}
+    /**
+     *
+     * Appel la methode DELETE de membreDAO
+     *
+     * @param idMembre Le ID du membre que nous désirons enlever de la base de données
+     * @throws ServiceException En cas d'erreur lors de l'appel au DAO, une exception est levée.
+     */
+    public void delete(int idMembre) throws ServiceException {
 
-	/*
-	 *
-	 * !!! À compléter !!!
-	 *
-	 * public void emprunter(MembreDTO membreDTO) {
-	 *
-	 * // Voir si le membre existe réellement
-	 * if(getMembre().read(membreDTO.getIdMembre()) != null) {
-	 *
-	 * } }
-	 */
+        try {
+            getMembreDAO().delete(idMembre);
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
+    }
 
-	/**
-	 * Suppression d'un membre dans la base de données.
-	 *
-	 * @param idMembre
-	 * @throws ServiceException
-	 */
-	public void desinscrire(int idMembre) throws ServiceException {
-		try {
+    // Opérations de recherche
 
-			// Instance du membre
-			MembreDTO tupleMembre = getMembre().read(idMembre);
+    /**
+     *
+     * Méthode retournant une liste de type <code>List</code> contenant des
+     * objets <code>MembreDTO</code>. La liste contient tous les membres
+     * enregistrés dans la base de données.
+     *
+     * @return liste une liste d'objets de type <code>MembreDTO</code>
+     *         représentant les membres enregistrés dans la base de données
+     *
+     * @throws ServiceException
+     */
+    public List<MembreDTO> getAll() throws ServiceException {
 
-			// Vérifie si le membre existe
-			if (tupleMembre == null) {
-				throw new ServiceException("Membre inexistant: " + idMembre);
-			}
+        try {
+            return getMembreDAO().getAll();
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
+    }
 
-			// Vérifier si le membre a encore des prêts en cours
-			if (tupleMembre.getNbPret() > 0) {
-				throw new ServiceException("Le membre " + idMembre + " a encore des prêts.");
-			}
+    // Méthodes métiers
 
-			/*
-			 * !!! À compléter !!!
-			 *
-			 * Vérifier si le membre a encore des réservations
-			 *
-			 * if (getReservation().getReservationMembre(idMembre) != null) {
-			 * throw new ServiceException("Membre " + idMembre +
-			 * " a des réservations"); }
-			 */
+    /**
+     *
+     * Vérifie si un membre existe
+     *
+     * @param idMembre
+     * @return
+     * @throws ServiceException
+     */
+    public boolean existe(int idMembre) throws ServiceException {
 
-			/* Suppression du membre */
-			getMembre().delete(idMembre);
+        // Un membre est-il trouvé? Si null, non: donc false
+        try {
+            return getMembreDAO().read(idMembre) != null ? true : false;
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
+    }
 
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
-	}
+    /**
+     * Ajout d'un nouveau membre dans la base de données. Si elle existe déjà,
+     * une exception est levée.
+     *
+     * @param idMembre
+     * @param nom
+     * @param telephone
+     * @param limitePret
+     * @throws ServiceException
+     */
+    public void inscrire(int idMembre,
+        String nom,
+        long telephone,
+        int limitePret) throws ServiceException {
 
-	/**
-	 * Getter de la variable d'instance <code>this.membre</code>.
-	 *
-	 * @return La variable d'instance <code>this.membre</code>
-	 */
-	public MembreDAO getMembre() {
-		return this.membre;
-	}
+        try {
 
-	/**
-	 * Setter de la variable d'instance <code>this.membre</code>.
-	 *
-	 * @param membre
-	 *            La valeur à utiliser pour la variable d'instance
-	 *            <code>this.membre</code>
-	 */
-	private void setMembre(MembreDAO membre) {
-		this.membre = membre;
-	}
+            if(getMembreDAO().read(idMembre) != null) {
 
-	/**
-	 * Getter de la variable d'instance <code>this.reservation</code>.
-	 *
-	 * @return La variable d'instance <code>this.reservation</code>
-	 */
-	public ReservationDAO getReservation() {
-		return this.reservation;
-	}
+                MembreDTO nouveauMembre = new MembreDTO();
+                nouveauMembre.setIdMembre(idMembre);
+                nouveauMembre.setNom(nom);
+                nouveauMembre.setTelephone(telephone);
+                nouveauMembre.setLimitePret(limitePret);
 
-	/**
-	 * Setter de la variable d'instance <code>this.reservation</code>.
-	 *
-	 * @param reservation
-	 *            La valeur à utiliser pour la variable d'instance
-	 *            <code>this.reservation</code>
-	 */
-	private void setReservation(ReservationDAO reservation) {
-		this.reservation = reservation;
-	}
+                add(nouveauMembre);
+            } else {
+                throw new ServiceException("Un membre avec l'id "
+                    + idMembre
+                    + " existe déjà");
+            }
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
 
-	/**
-	 *
-	 * Ajoute un membre à la table membre
-	 *
-	 * @param membreDTO
-	 * @throws ServiceException
-	 */
-	public void add(MembreDTO membreDTO) throws ServiceException {
+    }
 
-		try {
-			getMembre().add(membreDTO);
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
-	}
+    /*
+     *
+     * !!! À compléter !!!
+     *
+     * public void emprunter(MembreDTO membreDTO) {
+     *
+     * // Voir si le membre existe réellement
+     * if(getMembre().read(membreDTO.getIdMembre()) != null) {
+     *
+     * } }
+     */
 
-	/**
-	 *
-	 * Lit et renvoie un membre de la table membre
-	 *
-	 * @param idMembre
-	 * @return MembreDTO Le membre dont L'ID est spécifié
-	 * @throws ServiceException
-	 */
-	public MembreDTO read(int idMembre) throws ServiceException {
+    /**
+     * Suppression d'un membre dans la base de données.
+     *
+     * @param idMembre
+     * @throws ServiceException
+     */
+    public void desinscrire(int idMembre) throws ServiceException {
+        try {
 
-		try {
-			return getMembre().read(idMembre);
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
-	}
+            // Instance du membre
+            MembreDTO tupleMembre = getMembreDAO().read(idMembre);
 
-	/**
-	 *
-	 * Met à jour un membre de la table membre
-	 *
-	 * @param membreDTO
-	 * @throws ServiceException
-	 */
-	public void update(MembreDTO membreDTO) throws ServiceException {
+            // Vérifie si le membre existe
+            if(tupleMembre == null) {
+                throw new ServiceException("Membre inexistant: "
+                    + idMembre);
+            }
 
-		try {
-			getMembre().update(membreDTO);
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
-	}
+            // Vérifier si le membre a encore des prêts en cours
+            if(tupleMembre.getNbPret() > 0) {
+                throw new ServiceException("Le membre "
+                    + idMembre
+                    + " a encore des prêts.");
+            }
 
-	/**
-	 *
-	 * Supprime un membre de la table membre
-	 *
-	 * @param idMembre
-	 * @throws ServiceException
-	 */
-	public void delete(int idMembre) throws ServiceException {
+            /*
+             * !!! À compléter !!!
+             *
+             * Vérifier si le membre a encore des réservations
+             *
+             * if (getReservation().getReservationMembre(idMembre) != null) {
+             * throw new ServiceException("Membre " + idMembre +
+             * " a des réservations"); }
+             */
 
-		try {
-			getMembre().delete(idMembre);
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
-	}
+            /* Suppression du membre */
+            getMembreDAO().delete(idMembre);
 
-	/**
-	 *
-	 * Méthode retournant une liste de type <code>List</code> contenant des
-	 * objets <code>MembreDTO</code>. La liste contient tous les membres
-	 * enregistrés dans la base de données.
-	 *
-	 * @return liste une liste d'objets de type <code>MembreDTO</code>
-	 *         représentant les membres enregistrés dans la base de données
-	 *
-	 * @throws ServiceException
-	 */
-	public List<MembreDTO> getAll() throws ServiceException {
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException);
+        }
+    }
 
-		try {
-			return getMembre().getAll();
-		} catch (DAOException daoException) {
-			throw new ServiceException(daoException);
-		}
-	}
+    /**
+     * Getter de la variable d'instance <code>this.membreDAO</code>.
+     *
+     * @return La variable d'instance <code>this.membreDAO</code>
+     */
+    public MembreDAO getMembreDAO() {
+        return this.membreDAO;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.membreDAO</code>.
+     *
+     * @param membre
+     *            La valeur à utiliser pour la variable d'instance
+     *            <code>this.membreDAO</code>
+     */
+    private void setMembreDAO(MembreDAO membreDAO) {
+        this.membreDAO = membreDAO;
+    }
+
+    /**
+     * Getter de la variable d'instance <code>this.reservationDAO</code>.
+     *
+     * @return La variable d'instance <code>this.reservationDAO</code>
+     */
+    public ReservationDAO getReservationDAO() {
+        return this.reservationDAO;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.reservationDAO</code>.
+     *
+     * @param reservation
+     *            La valeur à utiliser pour la variable d'instance
+     *            <code>this.reservationDAO</code>
+     */
+    private void setReservationDAO(ReservationDAO reservationDAO) {
+        this.reservationDAO = reservationDAO;
+    }
+
 }
