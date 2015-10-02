ac3a8d8302ef0e95f6922b19007d62131245262d
bruno <bruno@baSs-PC>
2015-10-01 22:20:13 -0400

    Push commit qui remet le ReservationService à son état initial (backup du 30 septembre)

--- bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/ReservationService.java
+++ bibliotheque-02-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/ReservationService.java
@@ -8,8 +8,11 @@ import java.sql.Date;
 import ca.qc.collegeahuntsic.bibliotheque.dao.LivreDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dao.MembreDAO;
 import ca.qc.collegeahuntsic.bibliotheque.dao.ReservationDAO;
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
+import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.ReservationDTO;
-import ca.qc.collegeahuntsic.bibliotheque.exception.DAOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.ConnexionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.ServiceException;
 
 /**
@@ -40,6 +43,8 @@ public class ReservationService extends Services {
 
     private ReservationDAO reservation;
 
+    private Connexion cx;
+
     /**
      * Création d'une instance.
      * La connection de l'instance de livre et de membre doit être la même que cx,
@@ -50,8 +55,16 @@ public class ReservationService extends Services {
      * @param reservation
      * @throws ServiceException
      */
-    public ReservationService(ReservationDAO reservation) {
-        super();
+    public ReservationService(LivreDAO livre,
+        MembreDAO membre,
+        ReservationDAO reservation) throws ServiceException {
+        if(livre.getConnexion() != membre.getConnexion()
+            || reservation.getConnexion() != membre.getConnexion()) {
+            throw new ServiceException("Les instances de livre, de membre et de réservation n'utilisent pas la même connexion au serveur");
+        }
+        setCx(livre.getConnexion());
+        setLivre(livre);
+        setMembre(membre);
         setReservation(reservation);
     }
 
@@ -59,58 +72,66 @@ public class ReservationService extends Services {
      * Réservation d'un livre par un membre.
      * Le livre doit être prêté.
      *
-     * @param idReservation.
-     * @param idLivre.
+     * @param idReservation
+     * @param idLivre
      * @param idMembre
      * @param dateReservation
-     * @param reservationDTO
      * @throws ServiceException
      */
     public void reserver(int idReservation,
         int idLivre,
         int idMembre,
-        String dateReservation,
-        ReservationDTO reservationDTO) throws ServiceException {
+        String dateReservation) throws ServiceException {
         try {
             // Vérifie que le livre est prêté
-            if(getLivre().read(idLivre) == null) {
+            LivreDTO tupleLivre = getLivre().getLivre(idLivre);
+            if(tupleLivre == null) {
                 throw new ServiceException("Livre inexistant: "
                     + idLivre);
             }
-            if(getMembre().read(idMembre).getIdMembre() == 0) {
+            if(tupleLivre.getIdMembre() == 0) {
                 throw new ServiceException("Livre "
                     + idLivre
                     + " n'est pas prêté");
             }
-            if(getMembre().read(idMembre).getIdMembre() == idMembre) {
+            if(tupleLivre.getIdMembre() == idMembre) {
                 throw new ServiceException("Livre "
                     + idLivre
                     + " déjà prêté à ce membre");
             }
 
             // Vérifie que le membre existe
-            if(getMembre().read(idMembre) == null) {
+            MembreDTO tupleMembre = getMembre().getMembre(idMembre);
+            if(tupleMembre == null) {
                 throw new ServiceException("Membre inexistant: "
                     + idMembre);
             }
 
             // Vérifie si date reservation >= datePret
-            if(Date.valueOf(dateReservation).before(getLivre().read(idLivre).getDatePret())) {
+            if(Date.valueOf(dateReservation).before(tupleLivre.getDatePret())) {
                 throw new ServiceException("Date de réservation inférieure à la date de prêt");
             }
 
             // Vérifie que la réservation n'existe pas
-            if(getReservation().read(idReservation).getIdReservation() == idReservation) {
+            if(this.reservation.existe(idReservation)) {
                 throw new ServiceException("Réservation "
                     + idReservation
                     + " existe déjà");
             }
 
-            //Création de la réservation/
-            getReservation().add(reservationDTO);
+            //Création de la réservation
+            getReservation().reserver(idReservation,
+                idLivre,
+                idMembre,
+                dateReservation);
+            getCx().commit();
 
         } catch(Exception exception) {
-
+            try {
+                getCx().rollback();
+            } catch(ConnexionException connexionException) {
+                throw new ServiceException(connexionException);
+            }
             throw new ServiceException(exception);
         }
     }
@@ -123,15 +144,74 @@ public class ReservationService extends Services {
      *
      * @param idReservation
      * @param datePret
-     * @param idLivre
-     * @param idMembre
      * @throws ServiceException
      */
-    public ReservationDTO read(int idReservation) throws ServiceException {
+    public void prendreRes(int idReservation,
+        String datePret) throws ServiceException {
         try {
-            return getReservation().read(idReservation);
-        } catch(DAOException daoException) {
-            throw new ServiceException(daoException);
+            // Vérifie s'il y existe déjà une réservation pour le livre
+            ReservationDTO tupleReservation = getReservation().getReservation(idReservation);
+            if(tupleReservation == null) {
+                throw new ServiceException("Réservation inexistante : "
+                    + idReservation);
+            }
+
+            // Vérifie que c'est la première réservation pour le livre
+            ReservationDTO tupleReservationPremiere = getReservation().getReservationLivre(tupleReservation.getIdLivre());
+            if(tupleReservation.getIdReservation() != tupleReservationPremiere.getIdReservation()) {
+                throw new ServiceException("La réservation n'est pas la première de la liste "
+                    + "pour ce livre; la première est "
+                    + tupleReservationPremiere.getIdReservation());
+            }
+
+            // Vérifie si le livre est disponible
+            LivreDTO tupleLivre = getLivre().getLivre(tupleReservation.getIdLivre());
+            if(tupleLivre == null) {
+                throw new ServiceException("Livre inexistant: "
+                    + tupleReservation.getIdLivre());
+            }
+            if(tupleLivre.getIdMembre() != 0) {
+                throw new ServiceException("Livre "
+                    + tupleLivre.getIdLivre()
+                    + " déjà prêté à "
+                    + tupleLivre.getIdMembre());
+            }
+
+            // Vérifie si le membre existe et sa limite de prêt
+            MembreDTO tupleMembre = getMembre().getMembre(tupleReservation.getIdMembre());
+            if(tupleMembre == null) {
+                throw new ServiceException("Membre inexistant: "
+                    + tupleReservation.getIdMembre());
+            }
+            if(tupleMembre.getNbPret() >= tupleMembre.getLimitePret()) {
+                throw new ServiceException("Limite de prêt du membre "
+                    + tupleReservation.getIdMembre()
+                    + " atteinte");
+            }
+
+            // Vérifie si datePret >= tupleReservation.dateReservation
+            if(Date.valueOf(datePret).before(tupleReservation.getDateReservation())) {
+                throw new ServiceException("Date de prêt inférieure à la date de réservation");
+            }
+
+            // Enregistrement du prêt.
+            if(getLivre().preter(tupleReservation.getIdLivre(),
+                tupleReservation.getIdMembre(),
+                datePret) == 0) {
+                throw new ServiceException("Livre suprimé par une autre transaction");
+            }
+            if(getMembre().preter(tupleReservation.getIdMembre()) == 0) {
+                throw new ServiceException("Membre suprimé par une autre transaction");
+            }
+            // Éliminer la réservation.
+            getReservation().annulerRes(idReservation);
+            getCx().commit();
+        } catch(Exception exception) {
+            try {
+                getCx().rollback();
+            } catch(ConnexionException connexionException) {
+                throw new ServiceException(connexionException);
+            }
         }
     }
 
@@ -142,11 +222,25 @@ public class ReservationService extends Services {
      * @param idReservation
      * @throws ServiceException
      */
-    public void delete(ReservationDTO reservationDTO) throws ServiceException {
+    public void annulerRes(int idReservation) throws ServiceException {
         try {
-            getReservation().delete(reservationDTO);
-        } catch(DAOException daoException) {
-            throw new ServiceException(daoException);
+
+            // Vérifie que la réservation existe.
+            if(getReservation().annulerRes(idReservation) == 0) {
+                throw new ServiceException("Réservation "
+                    + idReservation
+                    + " n'existe pas");
+            }
+
+            getCx().commit();
+
+        } catch(Exception exception) {
+            try {
+                getCx().rollback();
+            } catch(ConnexionException connexionException) {
+                throw new ServiceException(connexionException);
+            }
+            throw new ServiceException(exception);
         }
     }
 
@@ -204,4 +298,21 @@ public class ReservationService extends Services {
         this.reservation = reservation;
     }
 
+    /**
+     * Getter de la variable d'instance <code>this.cx</code>.
+     *
+     * @return La variable d'instance <code>this.cx</code>
+     */
+    public Connexion getCx() {
+        return this.cx;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.cx</code>.
+     *
+     * @param cx La valeur à utiliser pour la variable d'instance <code>this.cx</code>
+     */
+    public void setCx(Connexion cx) {
+        this.cx = cx;
+    }
 }
