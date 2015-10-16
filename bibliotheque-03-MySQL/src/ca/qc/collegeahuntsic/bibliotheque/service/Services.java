520587564c77967e29a0b551ab624d37381dddf2
Laurent Boisvert <laurent.boisvert.1@collegeahuntsic.qc.ca>
2015-10-16 09:19:46 -0400

    First push version 3 - SQL

---
+++ bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/Services.java
@@ -0,0 +1,27 @@
+// Fichier Services.java
+// Auteur : Dragons Vicieux
+// Date de création : 2015-09-18
+
+package ca.qc.collegeahuntsic.bibliotheque.service;
+
+import java.io.Serializable;
+
+/**
+ *
+ * Classe de base pour tous les services.
+ *
+ * @author Dragons Vicieux
+ */
+public class Services implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     * Crée un service.
+     *
+     */
+    public Services() {
+    }
+
+}
