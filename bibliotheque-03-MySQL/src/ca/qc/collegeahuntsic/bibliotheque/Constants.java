6404067285c7b7a2b85847f9716b1d54cb8f3353
Laurent Boisvert <laurent.boisvert.1@collegeahuntsic.qc.ca>
2015-10-21 11:07:52 -0400

    Ajout contenu dans Constants

--- bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/Constants.java
+++ bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/Constants.java
@@ -1,9 +1,39 @@
 
 package ca.qc.collegeahuntsic.bibliotheque;
 
-public class Constants {
+/**
+ * Constants pour l'application Bibliothèque.
+ *
+ * @author Gilles Benichou
+ */
+public interface Constants {
+    /**
+     * Character for the empty character.
+     */
+    Character SPACE = new Character(' ');
 
-    Constants() {
+    /**
+     * Character for the character '['.
+     */
+    Character OPENING_BRACE = new Character('[');
 
-    }
+    /**
+     * Character for the character ']'.
+     */
+    Character CLOSING_BRACE = new Character(']');
+
+    /**
+     * Character for the character '='.
+     */
+    Character EQUALS = new Character('=');
+
+    /**
+     * Character for the character ','.
+     */
+    Character COMMA = new Character(',');
+
+    /**
+     * Character for the character '\n'.
+     */
+    Character NEW_LINE = new Character('\n');
 }
