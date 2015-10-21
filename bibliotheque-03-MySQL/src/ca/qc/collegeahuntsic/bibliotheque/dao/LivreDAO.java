98a175de5e634009bed377c7750d80053d3a856d
Bruno Lavigne <bruno.lavigne.1@collegeahuntsic.qc.ca>
2015-10-21 11:56:43 -0400

    modification LivreDAO requetes et preparedStatements

--- bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/LivreDAO.java
+++ bibliotheque-03-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/LivreDAO.java
@@ -16,7 +16,6 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
-import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.DAOException;
 
 /**
@@ -29,39 +28,27 @@ public class LivreDAO extends DAO {
 
     private static final long serialVersionUID = 1L;
 
-    private final static String READ_REQUEST = "SELECT idlivre, titre, auteur, dateAcquisition, idMembre, datePret "
+    private static final int NOMBRE_SEMAINES_EN_RETARD = 4;
+
+    private final static String READ_REQUEST = "SELECT idlivre, titre, auteur, dateAcquisition "
         + "FROM livre "
         + "WHERE idlivre = ?";
 
-    private final static String ADD_REQUEST = "INSERT into livre (idLivre, titre, auteur, dateAcquisition, idMembre, datePret) "
-        + "VALUES (?,?,?,?,null,null)";
+    private final static String ADD_REQUEST = "INSERT into livre (idLivre, titre, auteur, dateAcquisition) "
+        + "VALUES (?,?,?,?)";
 
-    private final static String UPDATE_REQUEST = "UPDATE livre set titre = ?, auteur = ?, dateAcquisition = ?, idMembre = ?, datePret = ? "
+    private final static String UPDATE_REQUEST = "UPDATE livre set titre = ?, auteur = ?, dateAcquisition = ? "
         + "WHERE idLivre = ?";
 
     private final static String DELETE_REQUEST = "DELETE from livre "
         + "WHERE idlivre = ?";
 
-    private final static String GET_ALL_REQUEST = "SELECT idLivre, titre, auteur, idmembre, datePret "
+    private final static String GET_ALL_REQUEST = "SELECT idLivre, titre, auteur "
         + "FROM livre";
 
-    private final static String FIND_BY_TITRE = "SELECT idLivre, titre, auteur, idmembre, datePret + 14 "
+    private final static String FIND_BY_TITRE = "SELECT idLivre, titre, auteur "
         + "FROM LIVRE "
         + "WHERE LOWER(titre) LIKE LOWER(?)";
-
-    private final static String FIND_BY_MEMBRE = "SELECT idLivre, titre, auteur, idmembre, datePret "
-        + "FROM livre "
-        + "WHERE idmembre = ?";
-
-    private final static String EMPRUNT_REQUEST = "UPDATE livre "
-        + "SET idMembre = ?, datePret = CURRENT_TIMESTAMP "
-        + "WHERE idLivre = ?";
-
-    private final static String RETOUR_REQUEST = "UPDATE livre "
-        + "SET idMembre = null, datePret = null "
-        + "WHERE idLivre = ?";
-    
-    private final static int NOMBRE_SEMAINES_EN_RETARD = 4;
 
     private final static String FIND_PRETS_EN_RETARD = "SELECT idLivre, titre, auteur, idmembre, datePret "
         + "FROM livre "
@@ -127,8 +114,6 @@ public class LivreDAO extends DAO {
                     tempLivre.setTitre(rset.getString(2));
                     tempLivre.setAuteur(rset.getString(3));
                     tempLivre.setDateAcquisition(rset.getTimestamp(4));
-                    tempLivre.setIdMembre(rset.getInt(5));
-                    tempLivre.setDatePret(rset.getTimestamp(6));
                     rset.close();
                 }
                 /*else {
@@ -162,10 +147,6 @@ public class LivreDAO extends DAO {
                 livreDTO.getAuteur());
             updatePreparedStatement.setTimestamp(4,
                 livreDTO.getDateAcquisition());
-            updatePreparedStatement.setInt(5,
-                livreDTO.getIdMembre());
-            updatePreparedStatement.setTimestamp(6,
-                livreDTO.getDatePret());
 
             updatePreparedStatement.executeUpdate();
         } catch(SQLException sqlException) {
@@ -216,8 +197,6 @@ public class LivreDAO extends DAO {
                         livreDTO.setIdLivre(resultSet.getInt(1));
                         livreDTO.setTitre(resultSet.getString(2));
                         livreDTO.setAuteur(resultSet.getString(3));
-                        livreDTO.setIdMembre(resultSet.getInt(4));
-                        livreDTO.setDatePret(resultSet.getTimestamp(5));
                         livres.add(livreDTO);
                     } while(resultSet.next());
                 }
@@ -261,49 +240,6 @@ public class LivreDAO extends DAO {
                     tempLivre.setIdLivre(rset.getInt(1));
                     tempLivre.setTitre(rset.getString(2));
                     tempLivre.setAuteur(rset.getString(3));
-                    tempLivre.setIdMembre(rset.getInt(4));
-                    tempLivre.setDatePret(rset.getTimestamp(5));
-                    liste.add(tempLivre);
-                    //listIsEmpty = false;
-                }
-                /*
-                if(listIsEmpty) {
-                    throw new DAOException("DAO-0003");
-                }
-                 */
-            }
-        } catch(SQLException sqlException) {
-            throw new DAOException(Integer.toString(sqlException.getErrorCode()),
-                sqlException);
-        }
-        return liste;
-    }
-
-    /**
-     *
-     * Trouve les livres à partir d'un membre.
-     *
-     * @param membreDTO - Le membre à utiliser.
-     * @return La liste des livres correspondants ; une liste vide sinon.
-     * @throws DAOException - S'il y a une erreur avec la base de données.
-     */
-    public List<LivreDTO> findByMembre(MembreDTO membreDTO) throws DAOException {
-        List<LivreDTO> liste = Collections.<LivreDTO> emptyList();
-        try(
-            PreparedStatement stmtGetLivresByMembre = getConnection().prepareStatement(LivreDAO.FIND_BY_MEMBRE);) {
-            stmtGetLivresByMembre.setInt(1,
-                membreDTO.getIdMembre());
-            try(
-                ResultSet rset = stmtGetLivresByMembre.executeQuery()) {
-                liste = new ArrayList<>();
-                //boolean listIsEmpty = true;
-                while(rset.next()) {
-                    LivreDTO tempLivre = new LivreDTO();
-                    tempLivre.setIdLivre(rset.getInt(1));
-                    tempLivre.setTitre(rset.getString(2));
-                    tempLivre.setAuteur(rset.getString(3));
-                    tempLivre.setIdMembre(rset.getInt(4));
-                    tempLivre.setDatePret(rset.getTimestamp(5));
                     liste.add(tempLivre);
                     //listIsEmpty = false;
                 }
@@ -334,104 +270,52 @@ public class LivreDAO extends DAO {
 
     /**
      *
-     * Emprunte un livre.
+     * Trouve les livres dont le prêt est en retard
      *
-     * @param livreDTO - Le livre à emprunter.
-     * @throws DAOException - S'il y a une erreur avec la base de données.
+     * @param dateJour - La date du jour
+     * @return List<LivreDTO> - La liste des livres en retard
+     * @throws DAOException - S'il y a une erreur avec la base de données
      */
-    public void emprunter(LivreDTO livreDTO) throws DAOException {
+    public List<LivreDTO> findPretsEnRetard(Date dateJour) throws DAOException {
+
+        // La liste contenant les livres en retard
+        List<LivreDTO> livresRetard = Collections.EMPTY_LIST;
+
         try(
-            PreparedStatement updatePreparedStatement = getConnection().prepareStatement(LivreDAO.EMPRUNT_REQUEST)) {
+            PreparedStatement statementGetAllLivresRetard = (getConnection().prepareStatement(LivreDAO.FIND_PRETS_EN_RETARD));) {
 
-            updatePreparedStatement.setInt(1,
-                livreDTO.getIdMembre());
-            updatePreparedStatement.setInt(2,
-                livreDTO.getIdLivre());
-            updatePreparedStatement.executeUpdate();
+            GregorianCalendar gregorianCalendar = new GregorianCalendar();
+            gregorianCalendar.setTime(dateJour);
+            gregorianCalendar.add(Calendar.WEEK_OF_MONTH,
+                -LivreDAO.NOMBRE_SEMAINES_EN_RETARD);
 
-        } catch(NullPointerException NPException) {
-            throw new DAOException("DAO-0004",
-                NPException);
+            Timestamp timestampRetard = new Timestamp(gregorianCalendar.getTimeInMillis());
+
+            statementGetAllLivresRetard.setTimestamp(1,
+                timestampRetard);
+
+            try(
+                ResultSet resultSet = statementGetAllLivresRetard.executeQuery()) {
+
+                LivreDTO livreDTO = null;
+
+                if(resultSet.next()) {
+                    livresRetard = new ArrayList<>();
+                    do {
+                        livreDTO = new LivreDTO();
+                        livreDTO.setIdLivre(resultSet.getInt(1));
+                        livreDTO.setTitre(resultSet.getString(2));
+                        livreDTO.setAuteur(resultSet.getString(3));
+                        livresRetard.add(livreDTO);
+                    } while(resultSet.next());
+                }
+            }
         } catch(SQLException sqlException) {
             throw new DAOException(Integer.toString(sqlException.getErrorCode()),
                 sqlException);
         }
+
+        return livresRetard;
+
     }
-
-    /**
-     *
-     * Retourne un livre.
-     *
-     * @param livreDTO - Le livre à retourner.
-     * @throws DAOException - S'il y a une erreur avec la base de données.
-     */
-    public void retourner(LivreDTO livreDTO) throws DAOException {
-        try(
-            PreparedStatement retourPreparedStatement = getConnection().prepareStatement(LivreDAO.RETOUR_REQUEST)) {
-
-            retourPreparedStatement.setInt(1,
-                livreDTO.getIdLivre());
-            retourPreparedStatement.executeUpdate();
-
-        } catch(NullPointerException NPException) {
-            throw new DAOException("DAO-0005",
-                NPException);
-        } catch(SQLException sqlException) {
-            throw new DAOException(Integer.toString(sqlException.getErrorCode()),
-                sqlException);
-        }
-    }
-    
-    /**
-    *
-    * Trouve les livres dont le prêt est en retard
-    *
-    * @param dateJour - La date du jour
-    * @return List<LivreDTO> - La liste des livres en retard
-    * @throws DAOException - S'il y a une erreur avec la base de données
-    */
-   public List<LivreDTO> findPretsEnRetard(Date dateJour) throws DAOException {
-
-       // La liste contenant les livres en retard
-       List<LivreDTO> livresRetard = Collections.EMPTY_LIST;
-
-       try(
-           PreparedStatement statementGetAllLivresRetard = (getConnection().prepareStatement(LivreDAO.FIND_PRETS_EN_RETARD));) {
-
-           GregorianCalendar gregorianCalendar = new GregorianCalendar();
-           gregorianCalendar.setTime(dateJour);
-           gregorianCalendar.add(Calendar.WEEK_OF_MONTH,
-               -LivreDAO.NOMBRE_SEMAINES_EN_RETARD);
-
-           Timestamp timestampRetard = new Timestamp(gregorianCalendar.getTimeInMillis());
-
-           statementGetAllLivresRetard.setTimestamp(1,
-               timestampRetard);
-
-           try(
-               ResultSet resultSet = statementGetAllLivresRetard.executeQuery()) {
-
-               LivreDTO livreDTO = null;
-
-               if(resultSet.next()) {
-                   livresRetard = new ArrayList<>();
-                   do {
-                       livreDTO = new LivreDTO();
-                       livreDTO.setIdLivre(resultSet.getInt(1));
-                       livreDTO.setTitre(resultSet.getString(2));
-                       livreDTO.setAuteur(resultSet.getString(3));
-                       livreDTO.setIdMembre(resultSet.getInt(4));
-                       livreDTO.setDatePret(resultSet.getTimestamp(5));
-                       livresRetard.add(livreDTO);
-                   } while(resultSet.next());
-               }
-           }
-       } catch(SQLException sqlException) {
-           throw new DAOException(Integer.toString(sqlException.getErrorCode()),
-               sqlException);
-       }
-
-       return livresRetard;
-
-   }
 }
