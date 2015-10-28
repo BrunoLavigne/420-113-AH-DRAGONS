bd01949a102678ce9ebd8783d25538bc7a9a2989
AliFadel <Ali.Fadel.1@collegeahuntsic.qc.ca>
2015-10-28 12:36:23 -0400

    implementation de la classe IDAO

--- bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IDAO.java
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IDAO.java
@@ -4,13 +4,113 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
 
+import java.io.Serializable;
+import java.util.List;
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.DTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
+
 /**
- * Interface de base pour tous les DAOs.<br/>
- * Toutes les interfaces de DAO devraient en hériter
  *
- * @author Dragons Vicieux
+ * TODO Auto-generated class javadoc
+ *
+ * @author Gilles Bénichou
  */
 public interface IDAO {
-    // TODO
+
+    /**
+     *
+     * ajoute un nouveau DTO dans la base de donnée
+     *
+     * @param connexion
+     * @param dto
+     * @throws InvalidHibernateSessionException
+     * @throws InvalidDTOException
+     * @throws InvalidDTOClassException
+     * @throws DAOException
+     */
+    void add(Connexion connexion,
+        DTO dto) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        DAOException;
+
+    /**
+     *
+     * TODO Auto-generated method javadoc
+     *
+     * @param connexion
+     * @param primaryKey
+     * @return
+     * @throws InvalidHibernateSessionException
+     * @throws InvalidPrimaryKeyException
+     * @throws DAOException
+     */
+    DTO get(Connexion connexion,
+        Serializable primaryKey) throws InvalidHibernateSessionException,
+        InvalidPrimaryKeyException,
+        DAOException;
+
+    /**
+     *
+     * TODO Auto-generated method javadoc
+     *
+     * @param connexion
+     * @param dto
+     * @throws InvalidHibernateSessionException
+     * @throws InvalidDTOException
+     * @throws InvalidDTOClassException
+     * @throws InvalidDTOClassException
+     * @throws DAOException
+     */
+    void update(Connexion connexion,
+        DTO dto) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        InvalidDTOClassException,
+        DAOException;
+
+    /* void save(Connexion connexion, DTO dto) throws InvalidHibernateSessionException,
+         InvalidDTOException,
+             InvalidDTOClassException,
+             DAOException;
+     */
+    /**
+     *
+     * TODO Auto-generated method javadoc
+     *
+     * @param connexion
+     * @param dto
+     * @throws InvalidHibernateSessionException
+     * @throws InvalidDTOException
+     * @throws InvalidDTOClassException
+     * @throws DAOException
+     */
+    void delete(Connexion connexion,
+        DTO dto) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        DAOException;
+
+    /**
+     *
+     * TODO Auto-generated method javadoc
+     *
+     * @param connexion
+     * @param sortByPropertyName
+     * @return
+     * @throws InvalidHibernateSessionException
+     * @throws InvalidSortByPropertyException
+     * @throws DAOException
+     */
+    List<? extends DTO> getAll(Connexion connexion,
+        String sortByPropertyName) throws InvalidHibernateSessionException,
+        InvalidSortByPropertyException,
+        DAOException;
 
 }
