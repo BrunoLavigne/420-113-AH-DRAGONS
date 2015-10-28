7d1105cb6c650ab499d77676ae2fac23ce86b8bb
AliFadel <Ali.Fadel.1@collegeahuntsic.qc.ca>
2015-10-28 15:51:37 -0400

    enlever tout les package-info pusique ils sont inutile

--- bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/util/package-info.java
+++
@@ -1,9 +0,0 @@
-// Fichier package-info.java
-// Auteur : Dragons Vicieux
-// Date de création : 2015-09-18
-/**
- *
- * @author Dragons Vicieux
- */
-
-package ca.qc.collegeahuntsic.bibliotheque.util;
