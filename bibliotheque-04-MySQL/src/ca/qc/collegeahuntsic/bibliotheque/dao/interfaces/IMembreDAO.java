162b469411e5a339d835b782fbf40227c61e66b8
Franz Nkezimana <Franz.Nkezimana.1@collegeahuntsic.qc.ca>
2015-10-28 12:47:33 -0400

    ajout de l'interface IMembreDAO

---
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/interfaces/IMembreDAO.java
@@ -0,0 +1,43 @@
+// Fichier IDAO.java
+// Auteur : Dragons Vicieux
+// Date de création : 2015-10-28
+
+package ca.qc.collegeahuntsic.bibliotheque.dao.interfaces;
+
+import java.util.List;
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+
+/**
+ *
+ * TODO Auto-generated class javadoc
+ *
+ * @author Gilles Bénichou
+ */
+public interface IMembreDAO extends IDAO {
+
+    /**
+     * Trouve les membres à partir d'un nom. La liste est classée par ordre croissant sur <code>sortByPropertyName</code>. Si aucun membre
+     * n'est trouvé, une {@link List} vide est retournée.
+     *
+     * @param connexion La connexion à utiliser
+     * @param nom Le nom à trouver
+     * @param sortByPropertyName Le nom de la propriété à utiliser pour classer
+     * @return La liste des membres correspondants ; une liste vide sinon
+     * @throws InvalidHibernateSessionException Si la connexion est <code>null</code>
+     * @throws InvalidCriterionException Si le nom est <code>null</code>
+     * @throws InvalidSortByPropertyException Si la propriété à utiliser pour classer est <code>null</code>
+     * @throws DAOException S'il y a une erreur avec la base de données
+     */
+    List<MembreDTO> findByNom(Connexion connexion,
+        String nom,
+        String sortByPropertyName) throws InvalidHibernateSessionException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        DAOException;
+
+}
