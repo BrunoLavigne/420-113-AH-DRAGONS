401eb1e34b3584d55165b3c19b1e015f297e1eac
Mensur <Mensur@Mensur_pc.ahuntsic.ca.intra>
2015-10-28 16:15:33 -0400

    IService est cree mais vide pour le moment, l'implementation de ILivreService est fais avec les methode qui doit etre fait

---
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/interfaces/IService.java
@@ -0,0 +1,16 @@
+// Fichier IPretDAO.java
+// Auteur : Dragons Vicieux
+// Date de création : 2015-10-28
+
+package ca.qc.collegeahuntsic.bibliotheque.service.interfaces;
+
+
+/**
+ * Interface DAO pour manipuler les prêts dans la base de données
+ *
+ * @author Dragons Vicieux
+ */
+public interface IService {
+
+    //TODO: CRUD
+}
