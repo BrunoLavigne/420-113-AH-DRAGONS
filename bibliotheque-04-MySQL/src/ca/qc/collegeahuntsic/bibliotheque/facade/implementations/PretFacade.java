94586f779661431fe18acfa4a05d9af4b27a8c74
Mohamed Nassim Laleg <Mohamed-Nassim.Laleg.1@collegeahuntsic.qc.ca>
2015-10-30 10:51:10 -0400

    modif pretFacade

--- bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/PretFacade.java
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/PretFacade.java
@@ -4,23 +4,131 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.facade.implementations;
 
-import java.io.Serializable;
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.PretDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.InvalidServiceException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingReservationException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.InvalidLoanLimitException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.MissingLoanException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ServiceException;
+import ca.qc.collegeahuntsic.bibliotheque.facade.interfaces.IPretFacade;
+import ca.qc.collegeahuntsic.bibliotheque.service.interfaces.IPretService;
 
 /**
  * Classe de base pour toutes les façades.
  *
  * @author Dragons Vicieux
  */
-public class PretFacade implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+public class PretFacade extends Facade implements IPretFacade {
+    private IPretService pretService;
 
     /**
-     * Crée une nouvelle Facade vide.
+     * Setter de la variable d'instance <code>this.pretService</code>.
      *
+     * @param pretService La valeur à utiliser pour la variable d'instance <code>this.pretService</code>
      */
-    public PretFacade() {
+    public void setPretService(IPretService pretService) {
+        this.pretService = pretService;
+    }
 
+    public PretFacade(IPretService pretService) throws InvalidServiceException { // TODO: Change to package when switching to Spring
+        super();
+        if(pretService == null) {
+            throw new InvalidServiceException("Le service de réservations ne peut être null");
+        }
+        setPretService(pretService);
+    }
+
+    // Region Getters and Setters
+    /**
+     * Getter de la variable d'instance <code>this.reservationService</code>.
+     *
+     * @return La variable d'instance <code>this.reservationService</code>
+     */
+    private IPretService getPretService() {
+        return this.pretService;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+
+    @Override
+    public void commencer(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        ExistingLoanException,
+        InvalidLoanLimitException,
+        ExistingReservationException,
+        InvalidDTOClassException,
+        FacadeException {
+        try {
+            getPretService().commencer(connexion,
+                pretDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void terminer(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        MissingLoanException,
+        ExistingLoanException,
+        InvalidDTOClassException,
+        FacadeException {
+        try {
+            getPretService().terminer(connexion,
+                pretDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void renouveler(Connexion connexion,
+        PretDTO pretDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        MissingLoanException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        ExistingLoanException,
+        ExistingReservationException,
+        FacadeException {
+        try {
+            getPretService().renouveler(connexion,
+                pretDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
     }
 
 }
