4a53163db31e9d46c81f6d16beb7a6216dff0cfd
franznk <kezfranz@yahoo.fr>
2015-10-30 10:35:50 -0400

    ajout des méthodes dans LivreFacde et MembreFacade(la doc n'est pas fini)

--- bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/MembreFacade.java
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/facade/implementations/MembreFacade.java
@@ -4,23 +4,102 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.facade.implementations;
 
-import java.io.Serializable;
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dto.MissingDTOException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.FacadeException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.facade.InvalidServiceException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingLoanException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ExistingReservationException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.service.ServiceException;
+import ca.qc.collegeahuntsic.bibliotheque.facade.interfaces.IMembreFacade;
+import ca.qc.collegeahuntsic.bibliotheque.service.interfaces.IMembreService;
 
 /**
  * Classe de base pour toutes les façades.
  *
  * @author Dragons Vicieux
  */
-public class MembreFacade implements Serializable {
+public class MembreFacade extends Facade implements IMembreFacade {
 
     private static final long serialVersionUID = 1L;
+
+    private IMembreService membreService;
 
     /**
      * Crée une nouvelle Facade vide.
      *
      */
-    public MembreFacade() {
+    public MembreFacade(IMembreService membreService) throws InvalidServiceException {// TODO changer la visibilité a package quand nous aurons la version avec Spring
+        super();
+        if(membreService == null) {
+            throw new InvalidServiceException("Le service des membres ne peut être null");
+        }
+        setMembreService(membreService);
+    }
 
+    /**
+     * Getter de la variable d'instance <code>this.membreService</code>.
+     *
+     * @return La variable d'instance <code>this.membreService</code>
+     */
+    public IMembreService getMembreService() {
+        return this.membreService;
+    }
+
+    /**
+     * Setter de la variable d'instance <code>this.membreService</code>.
+     *
+     * @param membreService La valeur à utiliser pour la variable d'instance <code>this.membreService</code>
+     */
+    public void setMembreService(IMembreService membreService) {
+        this.membreService = membreService;
+    }
+
+    /*
+     * {@inheritDoc}
+     */
+    @Override
+    public void inscrire(Connexion connexion,
+        MembreDTO membreDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        FacadeException {
+        try {
+            getMembreService().inscrire(connexion,
+                membreDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
+    }
+
+    /*
+     * {@inheritDoc}
+     */
+    @Override
+    public void desinscrire(Connexion connexion,
+        MembreDTO membreDTO) throws InvalidHibernateSessionException,
+        InvalidDTOException,
+        InvalidDTOClassException,
+        InvalidPrimaryKeyException,
+        MissingDTOException,
+        ExistingLoanException,
+        InvalidCriterionException,
+        InvalidSortByPropertyException,
+        ExistingReservationException,
+        FacadeException {
+        try {
+            getMembreService().desinscrire(connexion,
+                membreDTO);
+        } catch(ServiceException serviceException) {
+            throw new FacadeException(serviceException);
+        }
     }
 
 }
