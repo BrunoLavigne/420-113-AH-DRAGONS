4c6a553461fc1922f9223207e66c9a1a29dad3e1
Marc-Éric Boury <Marc-Eric.Boury.1@collegeahuntsic.qc.ca>
2015-10-30 10:06:05 -0400

    Completed LivreService

--- bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/implementations/LivreService.java
+++ bibliotheque-04-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/service/implementations/LivreService.java
@@ -8,10 +8,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.List;
-import ca.qc.collegeahuntsic.bibliotheque.dao.implementations.LivreDAO;
-import ca.qc.collegeahuntsic.bibliotheque.dao.implementations.MembreDAO;
-import ca.qc.collegeahuntsic.bibliotheque.dao.implementations.PretDAO;
-import ca.qc.collegeahuntsic.bibliotheque.dao.implementations.ReservationDAO;
+import ca.qc.collegeahuntsic.bibliotheque.dao.interfaces.ILivreDAO;
+import ca.qc.collegeahuntsic.bibliotheque.dao.interfaces.IMembreDAO;
+import ca.qc.collegeahuntsic.bibliotheque.dao.interfaces.IPretDAO;
+import ca.qc.collegeahuntsic.bibliotheque.dao.interfaces.IReservationDAO;
 import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
@@ -38,13 +38,13 @@ public class LivreService extends Services implements ILivreService {
 
     private static final long serialVersionUID = 1L;
 
-    private LivreDAO livreDAO;
+    private ILivreDAO livreDAO;
 
-    private MembreDAO membreDAO;
+    private IMembreDAO membreDAO;
 
-    private ReservationDAO reservationDAO;
+    private IReservationDAO reservationDAO;
 
-    private PretDAO pretDAO;
+    private IPretDAO pretDAO;
 
     /**
      *
@@ -56,10 +56,10 @@ public class LivreService extends Services implements ILivreService {
      * @param reservationDAO - Le DAO de la table <code>reservation</code>
      * @throws InvalidDAOException - Si le DAO de livre est <code>null</code>, si le DAO de membre est <code>null</code>, si le DAO de prêt est <code>null</code> ou si le DAO de réservation est <code>null</code>
      */
-    protected LivreService(LivreDAO livreDAO,
-        MembreDAO membreDAO,
-        PretDAO pretDAO,
-        ReservationDAO reservationDAO) throws InvalidDAOException {
+    protected LivreService(ILivreDAO livreDAO,
+        IMembreDAO membreDAO,
+        IPretDAO pretDAO,
+        IReservationDAO reservationDAO) throws InvalidDAOException {
         if(livreDAO == null) {
             throw new InvalidDAOException("Le DAO de la table livre est null");
         }
@@ -116,10 +116,11 @@ public class LivreService extends Services implements ILivreService {
             throw new InvalidPrimaryKeyException("La clé de recherche est null");
         }
         try {
-            return getLivreDAO().get(connexion,
+            return (LivreDTO) getLivreDAO().get(connexion,
                 idLivre);
-        } catch(DAOException daoexception) {
-            throw new ServiceException(daoexception);
+        } catch(DAOException daoException) {
+            throw new ServiceException(daoException.getMessage(),
+                daoException);
         }
     }
 
@@ -172,6 +173,7 @@ public class LivreService extends Services implements ILivreService {
     /**
      * {@inheritDoc}
      */
+    @SuppressWarnings("unchecked")
     @Override
     public List<LivreDTO> getAll(Connexion connexion,
         String sortByPropertyName) throws InvalidHibernateSessionException,
@@ -184,7 +186,7 @@ public class LivreService extends Services implements ILivreService {
             throw new InvalidSortByPropertyException("La propriété de recherche est null.");
         }
         try {
-            return getLivreDAO().getAll(connexion,
+            return (List<LivreDTO>) getLivreDAO().getAll(connexion,
                 sortByPropertyName);
         } catch(DAOException daoexception) {
             throw new ServiceException(daoexception);
@@ -299,7 +301,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @return La variable d'instance <code>this.livreDAO</code>
      */
-    private LivreDAO getLivreDAO() {
+    private ILivreDAO getLivreDAO() {
         return this.livreDAO;
     }
 
@@ -308,7 +310,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @param livreDAO La valeur à utiliser pour la variable d'instance <code>this.livreDAO</code>
      */
-    private void setLivreDAO(LivreDAO livreDAO) {
+    private void setLivreDAO(ILivreDAO livreDAO) {
         this.livreDAO = livreDAO;
     }
 
@@ -317,7 +319,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @return La variable d'instance <code>this.membreDAO</code>
      */
-    private MembreDAO getMembreDAO() {
+    private IMembreDAO getMembreDAO() {
         return this.membreDAO;
     }
 
@@ -326,7 +328,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @param membreDAO La valeur à utiliser pour la variable d'instance <code>this.membreDAO</code>
      */
-    private void setMembreDAO(MembreDAO membreDAO) {
+    private void setMembreDAO(IMembreDAO membreDAO) {
         this.membreDAO = membreDAO;
         // TODO THIS LINE DOES BASICALLY NOTHING BUT BLOCKS THE METHOD getMembreDAO() FROM BEING DELETED ON SAVE
         // OTHERWISE THE SAID METHOD WAS UNUSED LOCALLY AND WOULD BE DELETED.
@@ -338,7 +340,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @return La variable d'instance <code>this.reservationDAO</code>
      */
-    private ReservationDAO getReservationDAO() {
+    private IReservationDAO getReservationDAO() {
         return this.reservationDAO;
     }
 
@@ -347,7 +349,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @param reservationDAO La valeur à utiliser pour la variable d'instance <code>this.reservationDAO</code>
      */
-    private void setReservationDAO(ReservationDAO reservationDAO) {
+    private void setReservationDAO(IReservationDAO reservationDAO) {
         this.reservationDAO = reservationDAO;
     }
 
@@ -356,7 +358,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @return La variable d'instance <code>this.pretDAO</code>
      */
-    public PretDAO getPretDAO() {
+    public IPretDAO getPretDAO() {
         return this.pretDAO;
     }
 
@@ -365,7 +367,7 @@ public class LivreService extends Services implements ILivreService {
      *
      * @param pretDAO La valeur à utiliser pour la variable d'instance <code>this.pretDAO</code>
      */
-    public void setPretDAO(PretDAO pretDAO) {
+    public void setPretDAO(IPretDAO pretDAO) {
         this.pretDAO = pretDAO;
     }
 
