4df92edc708ad2f5929ed1e646993f79261b273e
Bruno Lavigne <bruno.lavigne.1@collegeahuntsic.qc.ca>
2015-12-04 08:14:40 -0500

    Début junit

---
+++ bibliotheque-06-back-end-test/src/test/collegeahuntsic/bibliothequeBackEnd/exception/package-info.java
@@ -0,0 +1,11 @@
+// Fichier package-info.java
+// Auteur : Dragons Vicieux
+// Date de création : 2015-12-04
+/**
+ * TODO Auto-generated class javadoc.
+ *
+ * @author Bruno Lavigne
+ */
+
+package test.collegeahuntsic.bibliothequeBackEnd.exception;
+
---
+++ bibliotheque-06-back-end-test/src/test/collegeahuntsic/bibliothequeBackEnd/facade/TestLivreFacade.java
@@ -0,0 +1,14 @@
+// Fichier TestLivreFacade.java
+// Auteur : Dragons Vicieux
+// Date de création : 2015-12-04
+
+package test.collegeahuntsic.bibliothequeBackEnd.facade;
+
+/**
+ * TODO Auto-generated class javadoc
+ *
+ * @author Bruno Lavigne
+ */
+public class TestLivreFacade {
+
+}
